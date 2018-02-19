d1a7d9d14ff92b0889f25263d99d5d2121d23e0f
Md Azmal <azmalmd12@gmail.com>
2018-02-19 17:28:44 +0530

    added user not found exception

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from script import * # import User class
+import requests
 
 usr_name = input('Enter the User Name : ')
 
-newusr = User(usr_name)
-newusr.user_details()
-newusr.user_repos() 
+resp = requests.get('https://github.com/'+usr_name)
+
+if resp.status_code != 200: # check if user account exists
+	print("No such user") 
+else:
+	newusr = User(usr_name)
+	newusr.user_details()
+	newusr.user_repos() 
