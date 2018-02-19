8e7bee6da672c84968241430503a4c755463738d
Md. Azmal <azmalmd12@gmail.com>
2018-02-19 16:47:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ from script import * # import the class User
 Az = User('MD-AZMAL') # User object
 Az.user_details() # display user details
 Az.user_repos() # display repository details
+
+
+# Testing on other users
+
+Dan = User('shiffman')
+Dan.user_details()
+Dan.user_repos()
